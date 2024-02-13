62fcf4b42c7d8dde729ba5273c1882a0c8e1c823
karthi9772 <karthi.g9772@gmail.com>
2024-02-13 20:45:06 +0530

    fetched GamesList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {Grid,GridItem,Show} from "@chakra-ui/react"
 import { Navbar } from "./components/Navbar"
+import { GameGrid } from "./components/GameGrid"
 
 export const App = () => {
   return (
@@ -9,7 +10,7 @@ export const App = () => {
     lg: `"nav nav" "aside main"`
   }} >
     <GridItem area={"nav"} ><Navbar/></GridItem>
-    <GridItem area={"main"} >Main</GridItem>
+    <GridItem area={"main"} ><GameGrid /></GridItem>
     <Show above="lg">
       <GridItem area={"aside"} >Aside</GridItem>
     </Show>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,29 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
+import  apiClient  from "../Services/api-client"
+
+interface Game{
+  id: number,
+  name: string,
+}
+interface FetchGamesRes{
+  count: number,
+  results: Game[]
+}
+
 export const GameGrid = () => {
-    const[games,SetGames]=useState([])
+    const[games,SetGames]=useState<Game[]>([])
+    const [error,setError]=useState("");
+    useEffect(()=>{
+        apiClient.get<FetchGamesRes>('/games').then(res=>SetGames(res.data.results))
+        .catch(err=>setError(err.message))
+    })
   return (
-    <div>GameGrid</div>
+   <div>
+    <ul>
+      {games.map((game)=><li key={game.id}>{game.name}</li>
+      )}
+    </ul>
+    {error&&<h1>{error}</h1>}
+   </div>
   )
 }
