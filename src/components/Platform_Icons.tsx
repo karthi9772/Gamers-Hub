01852b09210d2db79ca3f4f40b2a445e0854b097
karthi9772 <karthi.g9772@gmail.com>
2024-02-17 18:45:14 +0530

    Imported Icons For Platforms

--- src/components/Platform_Icons.tsx
+++ src/components/Platform_Icons.tsx
@@ -1,14 +1,32 @@
+import {FaWindows,FaXbox,FaPlaystation,FaLinux,FaAndroid,FaApple} from "react-icons/fa"
+import {SiNintendo} from "react-icons/si"
+import {MdPhoneIphone} from "react-icons/md"
+import { BsGlobe } from "react-icons/bs"
 import { platform } from "../hooks/useGame"
-import {Text} from "@chakra-ui/react"
+import {HStack, Icon} from "@chakra-ui/react"
+import { IconType } from "react-icons"
 interface props{
    platforms: platform[]
 }
 
 export const Platform_Icons = ({platforms}: props) => {
+    const iconObj: {[key: string]: IconType}= {
+        pc:FaWindows,
+        playstation:FaPlaystation,
+        xbox:FaXbox,
+        linux:FaLinux,
+        android:FaAndroid,
+        nintendo:SiNintendo,
+        ios:MdPhoneIphone,
+        web:BsGlobe,
+        mac:FaApple
+    }
   return (
-    <>
-     <Text>{platforms.map((platform)=><Text key={platform.id}>{platform.name}</Text>)}</Text>
-    </>
+    <HStack marginY={3}>
+     {platforms.map((platform)=>
+     <Icon color='blue.500' as={iconObj[platform.slug]}></Icon>
+     )}
+    </HStack>
     
   )
 }
